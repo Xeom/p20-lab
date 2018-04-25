0aa8bba2d214d34875dbf0ef1ec9dc4811741798
Xeom <fwharf.fw@gmail.com>
2018-04-25 21:21:47 +0100

    Added window

--- window.h
+++ window.h
@@ -1,14 +1,9 @@
 #if !defined(WINDOW_H)
 # define WINDOW_H
-# include <QMouseEvent>
 # include <QWidget>
-# include <QLabel>
-# include <QPainter>
-# include <QPicture>
-
-# include <map>
-
-# include "line.h"
+# include <QGridLayout>
+# include "board.h"
+# include "colourpicker.h"
 
 using namespace std;
 
@@ -17,26 +12,13 @@ class Window : public QWidget
     Q_OBJECT
 
 protected:
-    map<int, Line> lines;
-    Pos            mouse_anchor;
-    int            maxid;
-
-    void paintEvent(QPaintEvent *event);
-    void mouseReleaseEvent(QMouseEvent *event);
-    void mousePressEvent(QMouseEvent *event);
-    void mouseMoveEvent(QMouseEvent *event);
-
-    void mouse_line(Pos fin);
-    void mouse_del(Pos p);
-    void clear(void);
+    Board *board;
+    ColourPicker *picker;
+    QGridLayout *grid;
 
 public:
+    Board *get_board(void);
     Window(QWidget *parent = 0);
-
-    void new_line(Line &line, int id);
-    void del_line(int id);
-    void draw_all(void);
-    void draw_line(int id);
 };
 
 #endif
